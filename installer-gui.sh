d1a909e1b3d55babf1ab100d5479ee4640ba8b11
mudler <mudler@dark-lab.net>
2015-10-01 13:58:37 +0200

    [installer-*] moving logic from text to gui, text is dropped for good

--- installer-gui.sh
+++ installer-gui.sh
@@ -3,7 +3,7 @@
 . /sbin/sabayon-functions.sh
 
 if sabayon_is_gui_install; then
-	sabayon_setup_autologin
-	/usr/libexec/installer-text.sh	
+	#sabayon_setup_autologin
+	/usr/bin/xinit /usr/bin/calamares-pkexec -- -nolisten tcp -br +bs -dpi 96 vt$XDG_VTNR
 	#sabayon_setup_gui_installer
 fi
